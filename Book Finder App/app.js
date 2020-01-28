a086c9545df0e096a66ff8045a1b475bbca37d58
sprashanthv <prashanth.v@vassarlabs.com>
2020-01-28 18:31:54 +0530

    Validation+ Bug Fixes - Book Finder Version 2

--- Book Finder App/app.js
+++ Book Finder App/app.js
@@ -1,23 +1,50 @@
 //DOM Queries
 const searchForm = document.querySelector('.search-form');
 const booksList = document.querySelector('.books-list');
+const loader = document.querySelector('.loader');
+const addToWishList = document.querySelector('.wishlist');
 
 //Class instances
 const book = new Book();
 
+//Utility functions
+checkIfPresent = field => {
+  let result = null;
+  result = field ? (field instanceof Array ? field.toString() : field) : 'Not Found';
+  return result;
+}
+
+addLoadingIcon = () => {
+  let html = `<i class="fa fa-spinner fa-2x fa-spin text-center text-white"></i>`;
+  loader.innerHTML = html;
+}
+
+removeLoadingIcon = () => {
+  loader.innerHTML = '';
+}
+
 searchForm.addEventListener('submit', e => {
   e.preventDefault();
+  book.clearData();
+  addLoadingIcon();
   if (e.target.tagName === 'BUTTON' || 'I') {
     let searchTerm = e.target.searchText.value.trim();
-
-    fetchBookData();
 
     book.fetchData(searchTerm)
       .then(data => {
         if(data.totalItems != 0) {
           let { items } = data;
             if(items !== undefined) {
+              removeLoadingIcon();
               items.forEach(item => {
+                item.volumeInfo.publisher = checkIfPresent(item.volumeInfo.publisher);
+                item.volumeInfo.authors = checkIfPresent(item.volumeInfo.authors);
+                item.volumeInfo.categories = checkIfPresent(item.volumeInfo.categories);
+                item.volumeInfo.pageCount = checkIfPresent(item.volumeInfo.pageCount);
+                item.volumeInfo.publishedDate = checkIfPresent(item.volumeInfo.publishedDate);
+                item.volumeInfo.averageRating = checkIfPresent(item.volumeInfo.averageRating);
+                item.volumeInfo.description = checkIfPresent(item.volumeInfo.description);
+                item.volumeInfo.language = item.volumeInfo.language ? langCodes[item.volumeInfo.language].nativeName : 'Not Found';
                 book.renderData(item);
               })
           }
--- Book Finder App/books.js
+++ Book Finder App/books.js
@@ -1,3 +1,6 @@
+//DOM Queries and Variables;
+let bookItem = '';
+
 class Book {
   constructor() {
     this.url = 'https://www.googleapis.com/books/v1/volumes?q='
@@ -11,6 +14,16 @@ class Book {
   }
 
   renderData(data) {
+    let { title, 
+          authors, 
+          publisher, 
+          publishedDate, 
+          description, 
+          categories, 
+          pageCount,
+          averageRating,
+          language
+        } = data.volumeInfo;
     let html = `
       <div class="book-item d-flex justify-content-between">
         <div>
@@ -19,21 +32,62 @@ class Book {
         <div class="book-details">
          <p>
             <strong class="item-label">Title:</strong> 
-            <span class="text-white">${data.volumeInfo.title}</span>
+            <span class="text-white">${title}</span>
          </p>
         <p>
           <strong class="item-label">Author:</strong> 
-          <span class="text-white">${data.volumeInfo.authors[0]}</span>
+          <span class="text-white">${authors}</span>
         </p>
         <p>
           <strong class="item-label">Publisher: </strong> 
-          <span class="text-white">${data.volumeInfo.publisher}</span>
+          <span class="text-white">${publisher}</span>
         </p>
+        <p>
+          <strong class="item-label">Published Date: </strong> 
+          <span class="text-white">${publishedDate}</span>
+        </p>
+        <p>
+          <strong class="item-label">Description: </strong> 
+          <span class="text-white description" data-toggle="tooltip" data-placement="top" title="${description}">
+            ${description.substring(0,20)}...
+          </span>
+        </p>
+        <p>
+          <strong class="item-label">Category: </strong> 
+          <span class="text-white">${categories}</span>
+        </p>
+        <p>
+          <strong class="item-label">Page Count: </strong> 
+          <span class="text-white">${pageCount}</span>
+        </p>
+        <p>
+          <strong class="item-label">Average Rating : </strong> 
+          <span class="text-white">${averageRating}</span>
+        </p>
+        <p>
+          <strong class="item-label">Language : </strong> 
+          <span class="text-white">${language}</span>
+        </p>
+      </div>
+      <div class="custom-checkbox d-flex justify-content-end">
+       <input type ="checkbox">
       </div>
     `;
 
+    bookItem = document.querySelector('.book-item');
+
+    handleCheckEvent(bookItem);
+
     booksList.innerHTML += html;
 
+  }
+
+  clearData() {
+    booksList.innerHTML = '';
+  }
+
+  handleCheckEvent(bookItem){
+    
   }
 
 }
