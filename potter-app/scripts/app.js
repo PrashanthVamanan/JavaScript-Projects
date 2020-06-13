e441bc6f45522cc64abd5d12cadd5c8e25c77522
sprashanthv <prashanthvamanan3@gmail.com>
2020-06-13 20:50:23 +0530

    Spells Page Css + Pagination

--- potter-app/scripts/app.js
+++ potter-app/scripts/app.js
@@ -1,22 +1,40 @@
+const currentPageTracker = document.querySelector('.current');
+const totalPageTracker = document.querySelector('.total');
+const prevButton = document.querySelector('.prev');
+const nextButton = document.querySelector('.next');
+
 let spellHolderContainer = null;
 let spellsData = null;
-let imageToPick = [1, 2, 3, 4, 5]
+let imageToPick = [1, 2, 3, 4, 5];
+
+let prevPage = 0;
+let currentPage = 1;
+let totalPages = null;
+
+currentPageTracker.textContent = currentPage;
+prevButton.style.display = 'none';
 
 function getSpellDetails() {
   getSpells()
     .then(data => {
       spellsData = data;
+      totalPages = Math.ceil(spellsData.length / 12);
+      totalPageTracker.textContent = totalPages;
+
       spellHolderContainer = document.querySelector('.spells-holder');
-      populateSpellsData();
+      let firstPageData = spellsData.slice(0, 12);
+      populateSpellsData(firstPageData);
     })
     .catch(err => {
       console.log("Error in fetching spells ", err);
     })
 }
 
-function populateSpellsData() {
-  spellsData.forEach(spell => {
+function populateSpellsData(pageData) {
 
+  spellHolderContainer.innerHTML = '';
+
+  pageData.forEach(spell => {
     let imageId = imageToPick[Math.floor(Math.random() * imageToPick.length)];
 
     let html = `
@@ -33,4 +51,42 @@ function populateSpellsData() {
     `
     spellHolderContainer.innerHTML += html;
   })
+}
+
+const incrementPage = () => {
+  currentPage += 1;
+  if (currentPage < totalPages) {
+    currentPageTracker.textContent = currentPage;
+    nextButton.style.display = 'block';
+    prevButton.style.display = 'block';
+  } else if (currentPage === totalPages) {
+    currentPageTracker.textContent = currentPage;
+    nextButton.style.display = 'none';
+  }
+}
+
+const decrementPage = () => {
+  currentPage -= 1;
+  if (currentPage > 1) {
+    currentPageTracker.textContent = currentPage;
+    nextButton.style.display = 'block';
+    prevButton.style.display = 'block';
+  } else if (currentPage == 1) {
+    currentPageTracker.textContent = currentPage;
+    prevButton.style.display = 'none';
+  }
+}
+
+function populateNextSet(value) {
+  if (value) {
+    let nextPageRecordsStart = (currentPage - 1) * 12;
+    let nextPageRecordsEnd = (currentPage * 12);
+    let nextPageRecords = spellsData.slice(nextPageRecordsStart, nextPageRecordsEnd);
+    populateSpellsData(nextPageRecords);
+  } else {
+    let previousPageRecordsStart = (currentPage - 1) * 12;
+    let previousPageRecordsEnd = currentPage * 12;
+    let prevPageRecords = spellsData.slice(previousPageRecordsStart, previousPageRecordsEnd);
+    populateSpellsData(prevPageRecords);
+  }
 }
